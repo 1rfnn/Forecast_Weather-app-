c642ce760e4105f93ebfcf035feb4c68c90b5df5
irfan khan <ikha4949@gmail.com>
2024-09-21 20:23:42 +0530

    script.js

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ const apiUrl =
 const searchBox = document.querySelector(".search input");
 
 const searchBtn = document.querySelector(".search button");
-
 const weatherIcon = document.querySelector(".weather-icon");
 
 
@@ -19,32 +18,16 @@ async function checkWheather(city) {
   if (response.status == 404) {
     document.querySelector(".error").style.display = "block";
     document.querySelector(".weather").style.display = "none";
-  } else {
-    var data = await response.json();
+  } 
+  else {
+    let data = await response.json();
 
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML =
       Math.round(data.main.temp) + "*C";
     document.querySelector(".humidity").innerHTML = data.wind.speed + " km/h";
 
-    console.log(data.weather[0].main);
-    if (data.weather[0].main == "Clouds") {
-      weatherIcon.src = "img/clouds.png";
-    } 
-
-    else if (data.weather[0].main == "Clear") {
-      weatherIcon.src = "img/clear.png";
-    } 
-
-    else if (data.weather[0].main == "Rain") {
-      weatherIcon.src = "img/rain.png";
-    } 
     
-    else if (data.weather[0].main == "Drizzle") {
-      weatherIcon.src = "img/drizzle.png";
-    } else if (data.weather[0].main == "Mist") {
-      weatherIcon.src = "img/mist.png";
-    }
 
     document.querySelector(".weather").style.display = "block";
 
